f62d7528f7a226c13d5e04381438a1337d4e7d44
excalibur-kvrv <excalibur.krv@gmail.com>
2024-05-25 14:36:54 +0530

    completed stage 2

--- app/main.py
+++ app/main.py
@@ -11,7 +11,9 @@ def main():
     sys.stdout.flush()
 
     # Wait for user input
-    input()
+    command = input()
+    sys.stdout.write(f"{command}: command not found")
+    sys.stdout.flush()
 
 
 if __name__ == "__main__":
